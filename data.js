d2726cb52991d4e89c51706f270f399ac5de6339
Emmanuel Orozco <emmanuel@laboratoria.la>
2018-03-07 11:26:16 -0600

    solution v1

---
+++ data.js
@@ -0,0 +1,46 @@
+var data = {
+  products: [
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Nintendo Switch",
+      description: "Just another description",
+      id: 3,
+      price: 3900
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Play Station 4",
+      description: "Just another description",
+      id: 2,
+      price: 6900
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Play Station 3",
+      description: "Yes",
+      id: 1,
+      price: 6500
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Play Station 2",
+      description: "Yes",
+      id: 1,
+      price: 1000
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Play Station 1",
+      description: "Yes",
+      id: 1,
+      price: 9500
+    },
+    {
+      imageUrl: "http://dummyimage.com/100x100",
+      title: "Nintendo 64",
+      description: "Yes",
+      id: 1,
+      price: 700
+    }
+  ]
+};
